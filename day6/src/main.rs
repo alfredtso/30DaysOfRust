b26c76fce50c8bcd6350e22d9a2f8fd1a70d9889
Alfred Tso <alfredtso@gmail.com>
2021-09-13 23:45:30 +0800

    Lifetime

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -19,6 +19,11 @@ fn main() {
     let p3 = p1.mixup(p2);
 
     println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
+
+    let x = String::from("diu nei");
+    let y = String::from("lo mei");
+    println!("longest: {}", longest_with_an_announcement(&x, &y, String::from("diu")));
+
 }
 
 fn largest<T: PartialOrd + Copy>(list: &[T]) -> &T {
@@ -106,3 +111,68 @@ impl Summary for Tweet {
 pub fn notify(item: &impl Summary) {
     println!("Breaking news! {}", item.summarize());
 }
+
+use std::fmt::Display;
+
+struct Pair<T> {
+    x: T,
+    y: T,
+}
+
+impl<T> Pair<T> {
+    fn new(x: T, y: T) -> Self {
+        Self { x, y }
+    }
+}
+
+impl<T: Display + PartialOrd> Pair<T> {
+    fn cmd_display(&self) {
+        if self.x >= self.y {
+            println!("The largest memeber is x = {}", self.x);
+        } else {
+            println!("The largest member is y = {}", self.y);
+        }
+    }
+}
+
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
+fn longest_first<'a>(x: &'a str, y: &str) -> &'a str {
+    x
+}
+
+// Lifetime Annotations in Struct Definitions
+struct ImportantExcerpt<'a> {
+    part: &'a str,
+}
+
+fn important_excerpt() {
+    let novel = String::from("Call me Ishmael. Some year ago...");
+    let first_sentence = novel.split('.').next().expect("Could no find a '.'");
+    let i = ImportantExcerpt {
+        part: first_sentence,
+    };
+}
+
+fn longest_with_an_announcement<'a, T>(
+    x: &'a str,
+    y: &'a str,
+    ann: T,
+    ) -> &'a str
+where
+    T: Display,
+{
+    println!("Announcement! {}", ann);
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
