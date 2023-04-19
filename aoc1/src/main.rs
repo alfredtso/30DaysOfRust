752f630d234f41988f99ba9048cc2743969cdefb
Alfred <alfred.tso@itarle.com>
2023-04-19 19:12:15 +0800

    using reqwest

--- aoc1/src/main.rs
+++ aoc1/src/main.rs
@@ -4,6 +4,9 @@ use std::fs;
 
 use reqwest::Error;
 use reqwest::header;
+use reqwest::Url;
+
+use http::Method;
 
 #[tokio::main]
 async fn main() {
@@ -22,10 +25,11 @@ async fn main() {
         Err(error) => println!("error: {}", error),
     }
 
-    let f = fs::read_to_string(filename);
+    //let f = fs::read_to_string(filename);
+    let f = get_input().await;
     let start_time = Instant::now();
     let fi = f.expect("fds");
-    //part_one(&fi);
+    part_one(&fi);
     let end_time = Instant::now();
     let elapsed_time = end_time - start_time;
     println!("Elapsed time: {:?} millis", elapsed_time.as_millis());
@@ -35,13 +39,18 @@ async fn main() {
 pub async fn get_input() -> Result<String, Error> {
     println!("making request");
     let mut headers = header::HeaderMap::new();
-    let res = reqwest::get("https://adventofcode.com/2022/day/1/input").await?;
-    
-    eprintln!("Res: {:?} {}", res.version(), res.status());
-    eprintln!("Headers: {:?}\n", res.headers());
+    //let res = reqwest::get("https://adventofcode.com/2022/day/1/input").await?;
+    let url = Url::parse("https://adventofcode.com/2022/day/1/input").unwrap();
+    let res = reqwest::Client::new()
+        .request(Method::GET, url)
+        .header("Cookie", "session=53616c7465645f5f9bfa85fb001f6ccb7786754993a06f2aac0df50c03b570b45f53111e41e60d2950c3b4a7fc0938545519f14a6d2eab4289d2bf720ad8190b")
+        .send()
+        .await?;
+
+    println!("Res: {:?} {}", res.version(), res.status());
+    println!("Headers: {:?}\n", res.headers());
 
     let body = res.text().await?;
-    println!("body = {:?}", body);
 
     return Ok(body)
 }
